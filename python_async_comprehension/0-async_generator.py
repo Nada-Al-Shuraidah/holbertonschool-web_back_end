e3a9cbe8176c22e198a0f6c5a80c3fee4839bfa7
Nada-Al-Shuraidah <10718@holbertonstudents.com>
2025-07-28 15:05:36 +0000

    Python - Async Comprehension

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Coroutine that yields 10 random numbers between 0 and 10,
+one every second"""
+
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """Yields a random float between 0 and 10 every second, 10 times."""
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
